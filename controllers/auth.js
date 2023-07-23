2157f5e90e39987a8ade65eb01285842f97c7369
Chizycodes <okwudirejoy@gmail.com>
2023-07-23 17:54:45 +0100

    implement security measures

--- controllers/auth.js
+++ controllers/auth.js
@@ -65,7 +65,7 @@ exports.updateDetails = asyncHandler(async (req, res, next) => {
 	const fieldsToUpdate = {
 		name: req.body.name,
 		email: req.body.email,
-	}
+	};
 
 	const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
 		new: true,
@@ -172,3 +172,14 @@ const sendTokenResponse = (user, statusCode, res) => {
 
 	res.status(statusCode).cookie('token', token, options).json({ success: true, token });
 };
+
+// @desc    Log user out / clear cookie
+// @route   GET /api/v1/auth/logout
+// @access  Private
+exports.logout = asyncHandler(async (req, res, next) => {
+	res.cookie('token', 'none', {
+		expires: new Date(Date.now() + 10 * 1000),
+		httpOnly: true,
+	});
+	res.status(200).json({ success: true, data: {} });
+});
